682dbad55a4219d5b39e11781208fafa160c83ff
Harrison Vieira <hvieira@gruporcp.local>
2020-06-07 00:53:39 -0300

    config web pack

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
+const webpack = require('webpack')
+
+require('dotenv').config()
+
 module.exports = {
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        // Note: we provide webpack above so you should not `require` it
-        // Perform customizations to webpack config
-        // Important: return the modified config
-        config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
-        return config
-      }
-    env:{
-        API:process.env.API
-    }
+  webpack: config => {
+    const env = Object.keys(process.env).reduce((acc, curr) => {
+      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr])
+      return acc
+    }, {})
+
+    config.plugins.push(new webpack.DefinePlugin(env))
+
+    return config
+  }
 }
